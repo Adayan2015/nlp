c6b84d01b95c301d424ead9cb8730feab189b5b3
fanyanyan <fanyanyan@catchingmedia.com>
2018-11-23 18:32:28 +0800

    update nlp

--- nlp/knn.py
+++ nlp/knn.py
@@ -1,12 +1,12 @@
 # -*- coding:utf-8 -*-
 """
 @author:Fan Yanyan
-@file:test.py
+@file:knn.py
 @time:2018/11/22 14:13
 """
 import numpy as np
 from numpy import *
-
+import operator
 import matplotlib
 import matplotlib.pyplot as plt
 import math
@@ -17,12 +17,6 @@ mpl.rcParams['font.sans-serif'] = ['FangSong']  # 指定默认字体
 mpl.rcParams['axes.unicode_minus'] = False  # 解决保存图像是负号，显示为方块的问题。
 
 
-# 创建数据源，返回数据集和类标签
-# def creat_dataset():
-#     datasets = array([[8, 4, 2], [7, 1, 1], [1, 4, 4], [3, 0, 5]])  # 数据集
-#     labels = ['非常热', '非常热', '一般热', '一般热']  # 类标签
-#     return datasets, labels
-s
 # 创建数据源，返回数据集和类标签
 def creat_datasets():
     datasets = array([[8, 4, 2], [7, 1, 1], [1, 4, 4], [3, 0, 5], [9, 4, 2], [7, 2, 1], [1, 5, 4], [3, 1, 5]])  # 数据集
@@ -46,24 +40,21 @@ def analyze_data_plot(x, y):
 
 # 构造KNN分类器
 def knn_Classfier(newV, datasets, labels, k):
-    import operator
+
     # 1.计算样本数据与样本库数据之间的距离
     SqrtDist = EuclideanDistance3(newV, datasets)
     # 2.根据距离进行排序
     sortdDistindexs = SqrtDist.argsort(axis=0)
-    # print(sortdDistindexs)
     # 3.针对k个点，统计各个类别的数量
     classCount = {}  # 统计各个类别分别的数量
     for i in range(k):
         votelabel = labels[sortdDistindexs[i]]
-        # print(sortdDistindexs[i], votelabel)
         # 统计标签的键值对
         classCount[votelabel] = classCount.get(votelabel, 0) + 1
-    # print(classCount)
+
     # 4.投票机制,少数服从多数原则,输入类别
     # 对各个分类字典进行排序，降序，itemtgetter按照value排序
     sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
-    # print(str(newV) + ' KNN投票预测结果是：', sortedClassCount[0][0])
     return sortedClassCount[0][0]
 
 
@@ -85,40 +76,21 @@ def EuclideanDistance(instance1, instance2, length):
 def EuclideanDistance3(newV, datasets):
     # 获取数据向量的行向量维度和列向量维度
     rowsize, colsize = datasets.shape
-    # print(tile(newV, (rowsize, 1)))
 
     # 各特征向量间做差值
     diffMat = tile(newV, (rowsize, 1)) - datasets
-    # print(diffMat)
 
     # 对差值平方
     sqDiffMat = diffMat ** 2
-    # print(sqDiffMat)
 
     # 差值平方和进行开方
     sqDist = sqDiffMat.sum(axis=1) ** 0.5
-    # print(sqDist)
     return sqDist
-
-
-# 利用KNN分类器预测随机访客天气感知度
-def Predict_temperature():
-    # 创建数据集和类标签
-    datasets, labels = creat_dataset()
-    # 采访新游客
-    iceCream = float(input('Q:请问你今天吃了几个冰激凌？\n'))
-    drinkWater = float(input('Q:请问你今天喝了几瓶水？\n'))
-    palyAct = float(input('Q:请问你今天户外活动几个小时？\n'))
-
-    newV = [iceCream, drinkWater, palyAct]
-    res = knn_Classfier(newV, datasets, labels, 3)
-    print("该访客认为成都天气是：", res)
 
 
 if __name__ == '__main__':
     # 创建数据集和类标签
-    datasets, labels = creat_dataset()
-    # print("数据集：\n" + str(datasets), '\n类标签\n', labels)
+    datasets, labels = creat_datasets()
 
     # 可视化分析数据
     # print(type(datasets[:, 0]))
@@ -144,6 +116,4 @@ if __name__ == '__main__':
     vecs = array([[2, 4, 4], [3, 0, 0], [5, 7, 2]])
     for vec in vecs:
         res = knn_Classfier(vec, datasets, labels, 3)
-        print(str(vec) + ' KNN投票预测结果是：', res)
-
-    # KNN分类器  # newV = [2, 4, 0]  # knn_Classfier(newV, datasets, labels, 2)
+        print(str(vec) + ' KNN投票预测结果是：', '非常热' if res == 0 else '一般热')
--- nlp/knnsklearn.py
+++ nlp/knnsklearn.py
@@ -5,23 +5,35 @@
 @time:2018/11/22 18:21
 """
 from sklearn import neighbors
+from numpy import *
 import knn as KNN
 
-def knn_sklearn_predict():
+
+def knn_sklearn_predict(newV, datasets, labels):
     # 调用机器学习库knn分类器算法
     knn = neighbors.KNeighborsClassifier()
-    # 调用模块下的方法，返回数据特征集和类标签
-    datasets,labels = KNN.create_dateset()
     # 传入参数，特征数据，分类标签
-    knn.fit(datasets,labels)
-     # knn预测
-    predictRes = knn.predict([[2,4,0]])
-    print(predictRes)
-    print('该访客认为成都的天气是:\t','非常热' if predictRes[0] ==0 else '一般热')
+    knn.fit(datasets, labels)
+    # knn预测
+    predictRes = knn.predict([newV])
+
+    print('该访客认为成都的天气是:\t', '非常热' if predictRes[0] == 0 else '一般热')
     return predictRes
 
+
+# 利用KNN分类器预测随机访客天气感知度
+def Predict_temperature():
+    # 创建数据集和类标签
+    datasets, labels = KNN.creat_datasets()
+    # 采访新游客
+    iceCream = float(input('Q:请问你今天吃了几个冰激凌？\n'))
+    drinkWater = float(input('Q:请问你今天喝了几瓶水？\n'))
+    palyAct = float(input('Q:请问你今天户外活动几个小时？\n'))
+
+    newV = [iceCream, drinkWater, palyAct]
+    knn_sklearn_predict(newV, datasets, labels)
 
 
 
 if __name__ == '__main__':
-    knn_sklearn_predict()
+    Predict_temperature()
